88d8acc4fa820ec5e3a63c12af2c384f9fce8eb9
pavonism <spawlonka@outlook.com>
2025-04-27 18:28:52 +0200

    Add EDA and first GB results

---
+++ src/data.py
@@ -0,0 +1,62 @@
+from typing import Tuple
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+COLUMNS = {
+    "age": int,
+    "workclass": "category",
+    "fnlwgt": int,
+    "education": "category",
+    "education_num": int,
+    "marital-status": "category",
+    "occupation": "category",
+    "relationship": "category",
+    "race": "category",
+    "sex": "category",
+    "capital-gain": int,
+    "capital-loss": int,
+    "hours-per-week": int,
+    "native-country": "category",
+    "target": "category",
+}
+
+
+def load_dataset() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # ' ?'/'?' is a missing value marker
+    # We drop education because it is represented by education_num
+    df = pd.read_csv(
+        "../data/raw/adult/adult.data",
+        header=None,
+        names=COLUMNS.keys(),
+        index_col=False,
+        na_values=[" ?", "?"],
+        dtype=COLUMNS,
+    ).drop(columns=["education"])
+
+    X, y = df.loc[:, df.columns != "target"], df["target"]
+    X = impute_missing_data(X)
+
+    return X, y
+
+
+def impute_missing_data(X: pd.DataFrame):
+    cols_with_na = X.columns[X.isna().any()].tolist()
+    filler = DecisionTreeClassifier(max_depth=5)
+    unknown = X.loc[:, X.columns.isin(cols_with_na)].copy()
+
+    # One hot encoded
+    known = pd.get_dummies(X.loc[:, ~X.columns.isin(cols_with_na)], drop_first=True)
+
+    for col in cols_with_na:
+        y_known = unknown[col][~unknown[col].isnull()]
+        X_known, X_nan = (
+            known.loc[~unknown[col].isnull()],
+            known.loc[unknown[col].isnull()],
+        )
+        filler.fit(X_known, y_known)
+        unknown.loc[unknown[col].isnull(), col] = filler.predict(X_nan)
+
+    X_filled = pd.concat([known, unknown], axis=1)
+    X_filled = pd.get_dummies(X_filled)
+
+    return X_filled
